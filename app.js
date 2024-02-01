dd574ab4f7b7bd8fd5da4167489291943e4c0ef4
Philippa Burgess <113276341+philippaburgess@users.noreply.github.com>
2024-01-31 21:27:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -279,8 +279,8 @@ function updateAirQualityData() {
         if (!heatmapImageryProvider) {
               console.log('Creating new heatmap imagery provider'); // Log when creating a new provider
             heatmapImageryProvider = new Cesium.UrlTemplateImageryProvider({
-                  url: `https://airquality.googleapis.com/v1/mapTypes/${airQualityMapType}/heatmapTiles/{z}/{x}/{y}?key=${airQualityApiKey}`;
-            });
+                  url: `https://airquality.googleapis.com/v1/mapTypes/${airQualityMapType}/heatmapTiles/{z}/{x}/{y}?key=${airQualityApiKey}`
+            })
             viewer.imageryLayers.addImageryProvider(heatmapImageryProvider);
       }
     } else if (heatmapImageryProvider && viewer.imageryLayers.contains(heatmapImageryProvider)) {
